7185f4f9264bd96c7415802222143a84c8b9e3d0
Niklas Fasching <niklas.fasching@gmail.com>
2018-04-01 16:23:40 +0200

    Init

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject org.clojars.niklasfasching/headless "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [org.clojars.niklasfasching/yawc "0.1.0-SNAPSHOT"]])
